ee8dd075bdba1eb7ab2e449c9ff303d1d772ef8f
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-05-20 18:47:23 +0000

    not final q2

---
+++ CodeChef/Competitions/SnackDown 2017/codechef.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class codechef
+{
+
+	public static String check(int a[],int n)
+	{
+		int flag=0;
+		int i;
+		if(n%2==0)
+			return "no";
+		else if(a[0]!=1&& a[n-1]!=1)
+			return "no";
+		else
+		{
+			int mid_pos=(n-1)/2;
+			int mid_item=a[mid_pos];
+
+			for(i=0;i<mid_pos;i++)
+			{
+				if(a[i+1]-a[i]!=1)
+					return "no";
+				else
+					flag++;
+			}
+			if(flag==mid_pos)
+			{
+				for(i=mid_pos;i<n-1;i++)
+			{
+				if(a[i]-a[i+1]!=1)
+					return "no";
+				else
+					flag++;
+			}
+			if(flag==n-1)
+				return "yes";
+			else
+				return "no";
+			}
+			else
+				return "no";
+		}
+	}
+
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		int s,n;
+		s=sc.nextInt();
+		int count=0;
+		do
+		{
+			n=sc.nextInt();
+			int h[]=new int[n];
+			for (int i=0;i<n ;i++ ) 
+			{
+				h[i]=sc.nextInt();
+			}
+
+			String res=check(h,n);
+			System.out.println(res);
+			count++;
+	}while(count<s);
+	}
+}
