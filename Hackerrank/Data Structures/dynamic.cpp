4a154a4a2a3589935e8a97b782dd3c12e80ac914
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-06-02 16:36:47 +0000

    try again later

---
+++ Hackerrank/Data Structures/dynamic.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,q;
+    cin>>n>>q;
+    int** seq;
+    *seq=(int*) calloc(sizeof(int),n);
+    int lastans=0;
+    
+    return 0;
+}
