ca2500eaa02750d5f49f04fbbc36c1f299dc3b08
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-07-08 19:19:13 +0000

    update working

--- Hackerrank/Code-Heat/mario.java
+++ Hackerrank/Code-Heat/mario.java
@@ -11,25 +11,22 @@ class mario {
         for(int i=0;i<n;i++)
             steps[i]=sc.nextInt();
         int jumps=0;
-        //0 0 1 0 1 0 0 0
+        //0 1 0 1 0 1 0 0 n=8
+        //0 1 0 1 0 1 0 n=7
+        //0 0 1 0 1 0 0 n=7
         for(int i=0;i<n-1;)
         {   
-            if(steps[i+1]==0 && steps[i+2]==1)
-                {   
-                    jumps++;
-                    i++;
-                }
-            else if(steps[i+1]==1 && steps[i+2]==0)
+           
+            if(i+2!=n && steps[i+2] ==0 )
             {
-                jumps++;
                 i+=2;
+                jumps++;
             }
-            else if(steps[i+1]==0 && steps[i+2]==0)
+            else if(steps[i+1] == 0 && (i+2 == n || steps[i+2]==1 ))
             {
+                i++;
                 jumps++;
-                i+=2;
             }
-            else i++;
         }
         System.out.println(jumps);
     }
